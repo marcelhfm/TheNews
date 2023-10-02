90698fb6a79c98f98924e7b24f5d074a3a752194
marcelhfm <73190362+marcelhfm@users.noreply.github.com>
2023-10-02 22:55:25 +0200

    Feature/multiple features (#10)
    * client side pagination
    
    * multi language support
    
    * frontend sends requests with token
    
    * api is protected
    
    ---------
    
    Co-authored-by: Marcel Hofmann <marcelhofmann@Marcels-MBP.fritz.box>

--- frontend/web-app/src/pages/index/components/AuthenticatedContent.tsx
+++ frontend/web-app/src/pages/index/components/AuthenticatedContent.tsx
@@ -2,30 +2,52 @@ import { trpc } from "../../../utils/trpc";
 import { ArticleCard } from "./ArticleCard";
 import { LoadingSpinner } from "../../../components/LoadingSpinner";
 import { Text } from "@radix-ui/themes";
+import { Pagination } from "../../../components/Pagination";
+import { useState } from "react";
 
 export const AuthenticatedContent = () => {
   const newsQuery = trpc.news.useQuery();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const onPageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  // Set the number of items to display per page
+  const itemsPerPage = 10; // Adjust this based on your preference
+
+  // Calculate the start and end indices for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
   return (
     <>
       {newsQuery.isLoading ? (
         <LoadingSpinner />
       ) : (
-        newsQuery.data?.map((el) => (
-          <ArticleCard
-            key={el.id}
-            headingText={el.title}
-            detailLink={el.detailsLink}
-            source={el.source}
-          >
-            {el.firstSentence && (
-              <Text weight="regular" size="3">
-                {el.firstSentence}
-              </Text>
-            )}
-            {}
-          </ArticleCard>
-        ))
+        <>
+          {newsQuery.data?.slice(startIndex, endIndex).map((el) => (
+            <ArticleCard
+              key={el.id}
+              headingText={el.title}
+              detailLink={el.detailsLink}
+              source={el.source}
+            >
+              {el.firstSentence && (
+                <Text weight="regular" size="3">
+                  {el.firstSentence}
+                </Text>
+              )}
+            </ArticleCard>
+          ))}
+          <Pagination
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+            totalPages={
+              Math.ceil(newsQuery.data?.length || 0 / itemsPerPage) || 1
+            }
+          />
+        </>
       )}
     </>
   );
---
+++ frontend/web-app/src/utils/trpc.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import type { AppRouter } from "../../../../backend/react-facing-backend/src";
+import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const trpc = createTRPCReact<AppRouter>();
+
+interface ITrpcWrapper {
+  children: React.ReactNode;
+}
+
+export const TrpcWrapper = ({ children }: ITrpcWrapper) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL as string;
+  const { user, getAccessTokenSilently, isAuthenticated, isLoading } =
+    useAuth0();
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient, setTrpcClient] = useState<any>();
+
+  useEffect(() => {
+    const getAccessToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently();
+
+        setTrpcClient(() =>
+          trpc.createClient({
+            links: [
+              httpBatchLink({
+                url: backendUrl,
+
+                headers() {
+                  return {
+                    Authorization: `Bearer ${accessToken}`,
+                  };
+                },
+              }),
+            ],
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (isAuthenticated && !isLoading) {
+      getAccessToken();
+    }
+  }, [
+    getAccessTokenSilently,
+    user?.sub,
+    isAuthenticated,
+    isLoading,
+    setTrpcClient,
+  ]);
+
+  if (trpcClient) {
+    return (
+      <>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </trpc.Provider>
+      </>
+    );
+  } else {
+    return <>{children}</>;
+  }
+};
